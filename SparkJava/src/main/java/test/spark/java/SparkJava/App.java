a03a6700fdb6fa022b12f37028fce4af8a81e1ef
davidguerreromejias <Lenovo@T460-PC0E1XPQ>
2018-06-21 12:50:31 +0200

    First commit.

---
+++ SparkJava/src/main/java/test/spark/java/SparkJava/App.java
@@ -0,0 +1,13 @@
+package test.spark.java.SparkJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
