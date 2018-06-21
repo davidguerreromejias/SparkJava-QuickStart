9474e68fc1222cb5d897a2980513b5c0560651e8
davidguerreromejias <Lenovo@T460-PC0E1XPQ>
2018-06-21 13:10:11 +0200

    Created a job that counts the number of lines that appear the letters "a" and "b"

--- SparkJava/src/main/java/test/spark/java/SparkJava/Main.java
+++ SparkJava/src/main/java/test/spark/java/SparkJava/Main.java
@@ -11,8 +11,8 @@ public class Main
 {
     public static void main( String[] args )
     {
-    	String logFile = "YOUR_SPARK_HOME/README.md"; // Should be some file on your system
-        SparkSession spark = SparkSession.builder().appName("Simple Application").getOrCreate();
+    	String logFile = "C:\\Users\\Lenovo\\Documents\\RandomText.txt";
+        SparkSession spark = SparkSession.builder().master("local").appName("Count number of lines with x letter").getOrCreate();
         Dataset<String> logData = spark.read().textFile(logFile).cache();
 
         long numAs = logData.filter(s -> s.contains("a")).count();
