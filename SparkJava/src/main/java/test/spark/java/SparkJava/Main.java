f45ab1b7475e87559d19d15650a29205a2573f7f
davidguerreromejias <Lenovo@T460-PC0E1XPQ>
2018-06-25 12:18:31 +0200

    Create schema of dataframe

--- SparkJava/src/main/java/test/spark/java/SparkJava/Main.java
+++ SparkJava/src/main/java/test/spark/java/SparkJava/Main.java
@@ -3,14 +3,21 @@ package test.spark.java.SparkJava;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.*;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
+
 import static org.apache.spark.sql.functions.col;
 
 /**
@@ -29,7 +36,7 @@ public class Main
                 .map(name -> name.toString())
                 .collect(Collectors.toSet());
         
-        /*long numAs = 0;
+        long numAs = 0;
         long numBs = 0;
         for (String fileName : fileSet) {
             Dataset<String> tempDataset = spark.read().textFile(fileName).cache();
@@ -37,10 +44,18 @@ public class Main
             numBs += tempDataset.filter(s -> s.contains("b")).count(); 
         }
         
-        //Dataset<String> logData = spark.read().textFile(logFile).cache();*/
+        //create a dataset with a defined schema
+        datasetWithSchema(spark);
+        
+        
+        //Dataset<String> logData = spark.read().textFile(logFile).cache();
         // DATASET
-        Dataset<Row> df = spark.read().csv("C:\\Users\\Lenovo\\Documents\\fifa-world-cup\\WorldCupMatches.csv");
+        // with header
+        //Dataset<Row> df = spark.read().option("header", "true").csv("C:\\Users\\Lenovo\\Documents\\fifa-world-cup\\WorldCupMatches.csv");
         //df.show();
+        // without header
+        Dataset<Row> df = spark.read().csv("C:\\Users\\Lenovo\\Documents\\fifa-world-cup\\WorldCupMatches.csv");
+        df.show();
         //df.select("_c5", "_c6", "_c7", "_c8").show();
         //df.select("_c3").contains("Spain");
         
@@ -88,4 +103,32 @@ public class Main
 
         spark.stop();
     }
+
+	private static void datasetWithSchema(SparkSession spark) {
+		
+		JavaRDD<String> peopleRDD = spark.sparkContext()
+		  .textFile("C:\\Users\\Lenovo\\Documents\\fifa-world-cup\\WorldCupMatches.csv",1)
+		  .toJavaRDD();
+
+		// The schema is encoded in a string
+		String schemaString = "x1 x2 x3 x4 x5 x6 x7 x8 x9 x10 x11 x12 x13 x14 x15 x16 x17 x18 x19 x20";
+
+		// Generate the schema based on the string of schema
+		List<StructField> fields = new ArrayList<>();
+		for (String fieldName : schemaString.split(" ")) {
+		  StructField field = DataTypes.createStructField(fieldName, DataTypes.StringType, true);
+		  fields.add(field);
+		}
+		StructType schema = DataTypes.createStructType(fields);
+
+		// Convert records of the RDD (matches) to Rows
+		JavaRDD<Row> rowRDD = peopleRDD.map((Function<String, Row>) record -> {
+		  String[] attributes = record.split(",");
+		  return RowFactory.create(attributes[0], attributes[1].trim());
+		});
+
+		// Apply the schema to the RDD
+		Dataset<Row> matchesDataFrame = spark.createDataFrame(rowRDD, schema);
+
+	}
 }
